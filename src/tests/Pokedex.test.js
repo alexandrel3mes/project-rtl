c9df3abbae83f1a1c849430092095d426a98f25b
Alexandre <alexandreeduardolemes@gmail.com>
2022-03-22 18:29:50 -0300

    Requisito 5 feito

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testa o componente Pokedex', () => {
+  const pikachuSrc = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+
   it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
     renderWithRouter(<App />);
 
@@ -16,5 +19,142 @@ describe('Testa o componente Pokedex', () => {
 
     const nextBtn = screen.getByRole('button', { name: /próximo pokémon/i });
     expect(nextBtn).toBeInTheDocument();
+
+    userEvent.click(nextBtn);
+    const pokeImg = screen.getByRole('img');
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/4/43/Spr_5b_151.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png');
+
+    userEvent.click(nextBtn);
+    expect(pokeImg).toHaveAttribute('src', pikachuSrc);
+  });
+
+  it('Teste se é mostrado apenas um Pokemón por vez', () => {
+    renderWithRouter(<App />);
+
+    const pokeImg = screen.getAllByRole('img');
+    expect(pokeImg.length).toBe(1);
+  });
+
+  it('Teste se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(<App />);
+
+    const testId = 'pokemon-type-button';
+    const att = 'data-testid';
+
+    const allBtn = screen.getByRole('button', { name: /all/i });
+    expect(allBtn).toBeInTheDocument();
+
+    const nextBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    /* const allBtns = screen.getAllByRole('button');
+    expect(allBtns[0]).toBeInTheDocument();
+
+    userEvent.click(allBtns[0]);
+    const pokeImg = screen.getByRole('img');
+    expect(pokeImg).toHaveAttribute('src', pikachuSrc);
+
+    userEvent.click(allBtns[1]);
+    expect(pokeImg).toHaveAttribute('src', pikachuSrc);
+
+    userEvent.click(allBtns[2]);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
+
+    userEvent.click(allBtns[3]);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png');
+
+    userEvent.click(allBtns[4]);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png');
+
+    userEvent.click(allBtns[5]);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png');
+
+    userEvent.click(allBtns[6]);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/4/40/Spr_5b_143.png');
+
+    userEvent.click(allBtns[7]);
+    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png'); */
+    const electricBtn = screen.getByRole('button', { name: /electric/i });
+    expect(electricBtn).toBeInTheDocument();
+    expect(electricBtn).toHaveAttribute(att, testId);
+    userEvent.click(electricBtn);
+    expect(nextBtn).toBeDisabled();
+    expect(allBtn).toBeInTheDocument();
+
+    const fireBtn = screen.getByRole('button', { name: /fire/i });
+    expect(fireBtn).toBeInTheDocument();
+    expect(fireBtn).toHaveAttribute(att, testId);
+    userEvent.click(fireBtn);
+    expect(nextBtn).toBeEnabled();
+    expect(allBtn).toBeInTheDocument();
+
+    const bugBtn = screen.getByRole('button', { name: /bug/i });
+    expect(bugBtn).toBeInTheDocument();
+    expect(bugBtn).toHaveAttribute(att, testId);
+    userEvent.click(bugBtn);
+    expect(nextBtn).toBeDisabled();
+    expect(allBtn).toBeInTheDocument();
+
+    const poisonBtn = screen.getByRole('button', { name: /poison/i });
+    expect(poisonBtn).toBeInTheDocument();
+    expect(poisonBtn).toHaveAttribute(att, testId);
+    userEvent.click(poisonBtn);
+    expect(nextBtn).toBeDisabled();
+    expect(allBtn).toBeInTheDocument();
+
+    const psychicBtn = screen.getByRole('button', { name: /psychic/i });
+    expect(psychicBtn).toBeInTheDocument();
+    expect(psychicBtn).toHaveAttribute(att, testId);
+    userEvent.click(psychicBtn);
+    expect(nextBtn).toBeEnabled();
+    expect(allBtn).toBeInTheDocument();
+
+    const normalBtn = screen.getByRole('button', { name: /normal/i });
+    expect(normalBtn).toBeInTheDocument();
+    expect(normalBtn).toHaveAttribute(att, testId);
+    userEvent.click(normalBtn);
+    expect(nextBtn).toBeDisabled();
+    expect(allBtn).toBeInTheDocument();
+
+    const dragonBtn = screen.getByRole('button', { name: /dragon/i });
+    expect(dragonBtn).toBeInTheDocument();
+    expect(dragonBtn).toHaveAttribute(att, testId);
+    userEvent.click(dragonBtn);
+    expect(nextBtn).toBeDisabled();
+    expect(allBtn).toBeInTheDocument();
+  });
+
+  it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+
+    const nextBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+
+    const resetBtn = screen.getByRole('button', { name: /all/i });
+    expect(resetBtn).toBeInTheDocument();
+
+    userEvent.click(resetBtn);
+    expect(nextBtn).toBeEnabled();
+
+    const currImg = screen.getByRole('img');
+    expect(currImg).toHaveAttribute('src', pikachuSrc);
   });
 });
