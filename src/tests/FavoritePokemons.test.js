2d5b38936fff94303f5a6f2d0f547bd28d2eea4e
Alexandre <alexandreeduardolemes@gmail.com>
2022-03-22 17:00:41 -0300

    Requisito 3 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente Favorite Pokemóns', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/favorites');
+    expect(localStorage.getItem('favoritePokemonIds')).toBe(null);
+
+    const msg = screen.getByText(/no favorite pokemon found/i);
+    expect(msg).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const detailBtn = screen.getByRole('link', { name: /more details/i });
+    expect(detailBtn).toBeInTheDocument();
+    expect(detailBtn).toHaveAttribute('href', '/pokemons/25');
+
+    userEvent.click(detailBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+
+    const favAdd = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    expect(favAdd).toBeInTheDocument();
+    userEvent.click(favAdd);
+    expect(localStorage.getItem('favoritePokemonIds')).toBe('[25]');
+
+    history.push('/favorites');
+
+    const pokeImg = screen.getByRole('img', { name: /pikachu sprite/i });
+    expect(pokeImg).toBeInTheDocument();
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente Pokedex', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    renderWithRouter(<App />);
+
+    const mainHeading = screen.getByRole('heading', { name: /encountered pokémons/i });
+    expect(mainHeading).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido o próximo Pokémon da lista quando o botão é clicado.', () => {
+    renderWithRouter(<App />);
+
+    const nextBtn = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextBtn).toBeInTheDocument();
+  });
+});
